6b9be149695d601a8f0456eb4c20e48f574498c9
Webdeveloperintraining <sil22006@byui.edu>
2023-05-02 15:27:51 -0700

    Json file in progress

--- scripts/tableInformation.js
+++ scripts/tableInformation.js
@@ -1,14 +1,49 @@
 // const jsonData='http://forklift-advantage.com/json/especificaciones.json';
 const json='https://webdeveloperintraining.github.io/Forklift-Website/json/especificaciones.json';
+//const json='./json/especificaciones.json';
 async function waitForData (json){
     const getJsondata = await fetch(json);
     if (getJsondata.ok) {
         const jsonToData= await getJsondata.json();
-        doStuff(jsonToData);
+        gatheringData(jsonToData);
     }
 }
 
-function doStuff(data){
-    console.log(data);
+function gatheringData(data){
+    const paletAzul=data['propiedades'];
+    //insertingDataTables(paletAzul,document.getElementById('pallet'))
+
 }
-waitForData (json);
+/*
+function insertingDataTables(data,place){
+    let table_row=document.createElement("tr");
+    //let table_data1=document.createElement("td");
+    //let table_data2=document.createElement("td");
+    table_row.innerHTML=`<td>${gettipoDeControl}</td><td>${data.tipoDeTranspaleta}</td>`;
+    //table_row.table_data2.innerHTML='Prueba2';
+    place.appendChild(table_row);
+    //place.appendChild(table_data1);
+    //place.appendChild(table_data2);
+}
+*/
+waitForData(json);
+
+
+
+
+
+
+// const arraySparse = [1, 3, /* empty */, 7];
+// let numCallbackRuns = 0;
+
+// arraySparse.forEach((element) => {
+//   console.log({ element });
+//   numCallbackRuns++;
+// });
+
+// console.log({ numCallbackRuns });
+
+// // { element: 1 }
+// // { element: 3 }
+// // { element: 7 }
+// // { numCallbackRuns: 3 }
